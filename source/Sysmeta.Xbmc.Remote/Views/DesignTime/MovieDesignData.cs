547a750a2a038b03fe117ac1bc91f3381ac8a7ea
Fredrik Eriksson <fredrik.eriksson@thebc.se>
2011-05-30 22:23:45 +0200

    Fixed alignment issues

--- source/Sysmeta.Xbmc.Remote/Views/DesignTime/MovieDesignData.cs
+++ source/Sysmeta.Xbmc.Remote/Views/DesignTime/MovieDesignData.cs
@@ -8,21 +8,24 @@
         public MovieDesignData()
         {
             this.Year = 2002;
-            this.Runtime = 117;
+            this.Runtime = "1h 56min";
             this.Title = "Star Trek: Nemesis";
-            this.TagLine = "A generation's final journey... begins.";
+            this.Tagline = "A generation's final journey... begins.";
             this.Studio = "Paramount Pictures";
+            this.Genre = "Action / Adventure / Science Fiction / Thriller";
             this.Thumbnail = new BitmapImage() { UriSource = new Uri(@"C:/Users/fen/AppData/Roaming/XBMC/userdata/Thumbnails/Video/f/f84ca5a8.tbn") };
             this.Director = "Stuart Baird";
-            this.Plot =
-                "Break out your Vulcan ears: The crew of the USS Enterprise is back. This time around, the Next Generation crew members -- including Capt. Picard, Cmdr. Riker and, of course, Data -- are dispatched as peace ambassadors to their longtime foes, the Romulans. There, they encounter a mysterious new enemy that thwarts not only the armistice process, but Earth itself!";
+            this.Plot = "Break out your Vulcan ears: The crew of the USS Enterprise is back. This time around, the Next Generation crew members -- including Capt. Picard, Cmdr. Riker and, of course, Data -- are dispatched as peace ambassadors to their longtime foes, the Romulans. There, they encounter a mysterious new enemy that thwarts not only the armistice process, but Earth itself!";
+            this.MPAA = "PG-13";
         }
 
         public int Year { get; set; }
 
+        public string MPAA { get; set; }
+
         public BitmapImage Thumbnail { get; set; }
 
-        public int Runtime { get; set; }
+        public string Runtime { get; set; }
 
         public string Title { get; set; }
 
@@ -34,7 +37,7 @@
 
         public string Plot { get; set; }
 
-        public string TagLine { get; set; }
+        public string Tagline { get; set; }
 
         public string Studio { get; set; }
     }
